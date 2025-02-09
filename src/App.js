c1a01324d57070506bf78f0c791c90d910c804c2
Developer866 <opeyemijoseph866@gmail.com>
2025-02-09 22:47:16 +0100

    desktop view completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import "./App.css";
 import Card from "./component/card";
-import Data from "./data";  // Ensure correct path
+import Data from "./data"; // Ensure correct path
 
 function App() {
-  return (
-    <div className="App">
-      {Data.map((item, index) => (
-        <div key={index} className={`box box-${index + 1}`}>
-          <Card data={item} /> {/* Pass a single object */}
-        </div>
-      ))}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
